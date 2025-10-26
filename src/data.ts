8896577a9a4a3d9d8953433958810a5cd94ff1e8
annapanfilova322 <annapanfilova121@gmail.com>
2025-10-26 15:17:53 +0300

    Create data.ts
    Add data file

---
+++ src/data.ts
@@ -0,0 +1,35 @@
+// ВСЕ ДАННЫЕ САЙТА
+export const siteData = {
+  // ПАРОЛЬ ДЛЯ АДМИНКИ
+  adminPassword: "admin123",
+  
+  // ОТЗЫВЫ
+  testimonials: [
+    {
+      id: 1,
+      company: "ООО Рога и копыта", 
+      drive_link: "https://drive.google.com/test1"
+    },
+    {
+      id: 2,
+      company: "ИП Иванов",
+      drive_link: "https://drive.google.com/test2" 
+    }
+  ],
+  
+  // СЕРТИФИКАТЫ
+  certificates: [
+    {
+      id: 1,
+      title: "Сертификат адвоката",
+      image: "/certificates/cert1.jpg",
+      drive_link: "https://drive.google.com/cert1"
+    },
+    {
+      id: 2, 
+      title: "Диплом юриста", 
+      image: "/certificates/cert2.jpg",
+      drive_link: "https://drive.google.com/cert2"
+    }
+  ]
+};
