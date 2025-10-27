667b59e622671bceaaa63e4477295f8b1f10e035
annapanfilova322 <annapanfilova121@gmail.com>
2025-10-27 12:06:35 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,20 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Простой тестовый компонент
+function TestApp() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'Arial' }}>
+      <h1>✅ Тестовая страница работает!</h1>
+      <p>Если ты видишь это сообщение - сайт работает</p>
+      <p>Проблема в компонентах, а не в настройках</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TestApp />
+  </React.StrictMode>,
+)
