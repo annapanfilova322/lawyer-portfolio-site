b63ac9532e5af8873fae3935f019a8bcd6cad013
annapanfilova322 <annapanfilova121@gmail.com>
2025-10-27 12:15:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-  ],
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: true,
-    hmr: {
-      overlay: false // Disables the error overlay if you only want console errors
-    }
-  },
-}));
+  build: {
+    outDir: 'dist'
+  }
+});
